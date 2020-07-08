704bc24e788c7991e7b0a88c68e25bf78c5c13d3
Tiu Wee Han <deantiu56@gmail.com>
2020-07-08 14:33:39 +0800

    Make time update just before command

--- .zsh/prompt.sh
+++ .zsh/prompt.sh
@@ -13,9 +13,6 @@ source ~/.zsh/zsh-git-prompt/zshrc.sh
 # PS1="%(?.%F{green}√.%F{red}?%?)%f %B%{$fg[green]%}%n%{$fg[blue]%}@%{$fg[blue]%}%M %{$fg[cyan]%}%~%{$reset_color%} %# "
 PS1='%{$fg[cyan]%}[%D{%H:%M:%S}] %(?.%F{green}√.%F{red}?%?)%f %B%{$fg[green]%}%n %{$fg[blue]%}%~%{$reset_color%} $(git_super_status)%# '
 
-# Reset the prompt when a command is entered for accurate timestamp
-TMOUT=1
-
 redraw_tmout() {
     case "$WIDGET" in
             expand-or-complete|self-insert|up-line-or-beginning-search|down-line-or-beginning-search|backward-delete-char|.history-incremental-search-backward|.history-incremental-search-forward)
@@ -28,9 +25,19 @@ redraw_tmout() {
     esac
 }
 
-TRAPALRM() {
-  redraw_tmout
+# # Reset the prompt when a command is entered for accurate timestamp
+# TMOUT=1
+# 
+# # To update clock every second
+# TRAPALRM() {
+#  redraw_tmout
+# }
+
+function _reset-prompt-and-accept-line {
+  zle reset-prompt
+  zle .accept-line     # Note the . meaning the built-in accept-line.
 }
+zle -N accept-line _reset-prompt-and-accept-line
 
 function preexec() {
   __timer=$(($(print -P %D{%s%6.})/1000))
@@ -45,3 +52,4 @@ function precmd() {
     unset __timer
   fi
 }
+
