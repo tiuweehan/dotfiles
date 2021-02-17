ea543ed86e2df99c36df5bf624cfe7d8e1bf836c
Tiu Wee Han <contact@tiuweehan.com>
2021-02-17 15:38:02 +0800

    Move bindkey -e above keybindings

--- .dotconfig/zsh/keyboard.sh
+++ .dotconfig/zsh/keyboard.sh
@@ -1,7 +1,6 @@
-bindkey "^[[1;3C" forward-word
-bindkey "^[[1;3D" backward-word
-
 bindkey -e
+bindkey '^[[1;3C' forward-word
+bindkey '^[[1;3D' backward-word
 
 # Use emacs key bindings
 set -g mode-keys emacs
